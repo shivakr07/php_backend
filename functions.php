ea2a12b1d898c0698b68958025dd3a59c71e427c
evans <kumarshiva0707@gmail.com>
2024-05-16 00:48:53 +0530

    functions parameterized and non parameterized

---
+++ functions.php
@@ -0,0 +1,27 @@
+<!-- functions -->
+
+
+
+<?php
+
+function sayHello(){
+    echo "I say Hello";
+}
+
+sayHello();
+echo "<br/>";
+// --------------------------------------
+
+function sq($num){
+    return $num * $num;
+}
+
+echo "Square of 10 = " . sq(10);
+// --------------------------------------
+
+
+
+
+
+?> 
+
